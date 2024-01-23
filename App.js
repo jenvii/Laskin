ba7e0bbfb982d593b570c64edf4af0711c463fd8
Jenna <143401323+jenvii@user.noreply.github.com>
2024-01-23 15:18:25 +0200

    Tehtävä tehty

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+
 
 export default function App() {
+
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [result, setResult] = useState('');
+
+  const showSum = () => {
+    const sum = Number(number1) + Number(number2);
+    setResult(sum);
+  }
+
+  const showRemainder = () => {
+    const remainder = Number(number1) - Number(number2);
+    setResult(remainder);
+  }
+
+  // tein tämän vielä ylimääräisenä kokeiluna
+  const showMultiplication = () => {
+    const multiplication = Number(number1) * Number(number2);
+    setResult(multiplication);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Result: {result}</Text>
+        <TextInput
+          keyboardType='numeric'
+          style={{ width: 200, borderColor: 'pink', borderWidth: 2 }}
+          onChangeText={text => setNumber1(text)} />
+        <TextInput
+          keyboardType='numeric'
+          style={{ width: 200, borderColor: 'pink', borderWidth: 2 }}
+          onChangeText={text => setNumber2(text)} />
+      </View>
+      <View style={styles.flexItem}>
+        <Button title="+" onPress={showSum} />
+        <Button title="-" onPress={showRemainder} />
+        <Button title="x" onPress={showMultiplication} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +55,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  flexItem: {
+    flexDirection: 'row',
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  }
 });
